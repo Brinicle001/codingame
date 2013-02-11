f05368d84e68b0f76e3c5b54b5e73cb4fbcf90b1
Benoit Dunand-Laisin <benoit.dunand_laisin@laposte.net>
2013-02-11 19:10:50 +0100

    Test 2 OK

--- 20130129_test_2/main.cpp
+++ 20130129_test_2/main.cpp
@@ -2,62 +2,273 @@
 
 #include <iostream>
 #include <string>
-#include <stdlib.h>
-
-#define MAX_INPUT 100000
-#define MAX_BEGIN_DAY 1000000
-#define MAX_DURATION 1000
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+/* CLASS PROPAL --------------------------------------------- */
+class Propal
+{
+public:
+    unsigned int startDate;
+    unsigned int endDate;
+    unsigned int nOverlappedOri,nOverlappedOptimized;
+    bool isNew;
+    bool isOptimized,isOK;
+
+    Propal(const unsigned int start, const unsigned int duration) throw(int);
+    Propal(const unsigned int start, const unsigned int duration, const unsigned int nOverlapped) throw(int);
+    ~Propal();
+    bool sooner (const Propal &a) const;
+    bool operator< (const Propal &a) const;
+    bool isOverlappedWith(const Propal &a) const;
+    friend ostream &operator<<(ostream &flux, const Propal &a);
+};
+
+Propal::Propal(const unsigned int start, const unsigned int duration) throw(int) :
+    startDate(start),
+    endDate(start),
+    nOverlappedOri(0),
+    nOverlappedOptimized(0),
+    isNew(true),
+    isOptimized(false),
+    isOK(false)
+{
+    try
+    {
+        if (duration==0)
+            throw 1;
+        else
+            this->endDate=start+duration -1;
+    }
+    catch (int)
+    {
+        cerr << "Propal(" << start << "," << duration << ") illegal" << endl;
+    }
+}
+
+Propal::Propal(const unsigned int start, const unsigned int duration, const unsigned int nOverlapped) throw(int) :
+    startDate(start),
+    endDate(start),
+    nOverlappedOri(nOverlapped),
+    nOverlappedOptimized(nOverlapped),
+    isNew(true),
+    isOptimized(false),
+    isOK(false)
+{
+    try
+    {
+        if (duration==0)
+            throw 1;
+        else
+            this->endDate=start+duration -1;
+    }
+    catch (int)
+    {
+        cerr << "Propal(" << start << "," << duration << ") illegal" << endl;
+    }
+}
+
+Propal::~Propal() {}
+
+bool Propal::sooner (const Propal &a) const
+{
+    if (this->startDate==a.startDate)
+        return this->endDate<a.endDate;
+    else
+        return this->startDate<a.startDate;
+}
+
+bool Propal::operator< (const Propal &a) const
+{
+    return this->sooner(a);
+}
+
+bool Propal::isOverlappedWith(const Propal &a) const
+{
+    if ((this->startDate>=a.startDate) && (this->startDate<=a.endDate))
+    {
+        return true;
+    }
+    if ((this->endDate>=a.startDate) && (this->endDate<=a.endDate))
+    {
+        return true;
+    }
+    if ((this->startDate<a.startDate) && (this->endDate>a.endDate))
+    {
+        return true;
+    }
+    return false;
+}
+
+
+ostream &operator<<(ostream &flux, const Propal &a)
+{
+    flux << a.startDate << "," << a.endDate << " poids=" << a.nOverlappedOri << "/" << a.nOverlappedOptimized << " new=" << a.isNew << " opti=" << a.isOptimized << " OK?" << a.isOK;
+    return flux;
+}
+
+
+/* CLASS PROPAL_MANAGER ---------------------------------------- */
+typedef vector<Propal> VecPropal;
+struct FoncteurMinPropal
+{
+   inline bool operator() (const Propal &a, const Propal &b) const
+   {
+      return a.nOverlappedOptimized < b.nOverlappedOptimized;
+   }
+};
+
+class Propal_manager
+{
+public:
+    VecPropal Propals;
+    bool hasChanged;
+    unsigned int maxOccurs;
+    Propal_manager();
+    ~Propal_manager();
+    void addPropal(unsigned int start, unsigned int duration);
+    void calcOverlapped();
+    void optimize();
+    unsigned int getBestNumber();
+    friend istream &operator>>(istream &flux, Propal_manager &p);
+    friend ostream &operator<<(ostream &flux, const Propal_manager &p);
+};
+
+Propal_manager::Propal_manager() :
+    Propals(),
+    hasChanged(false),
+    maxOccurs(0) {}
+
+Propal_manager::~Propal_manager() {}
+
+void Propal_manager::addPropal(unsigned int start, unsigned int duration)
+{
+    this->Propals.push_back(Propal(start, duration));
+    this->hasChanged=true;
+}
+
+void Propal_manager::calcOverlapped()
+{
+    VecPropal::iterator currentPropal=this->Propals.begin();
+    VecPropal::iterator tmpIt=this->Propals.begin();
+    while(currentPropal != this->Propals.end())
+    {
+        tmpIt=currentPropal+1;
+        while((tmpIt->startDate<=currentPropal->endDate) && (tmpIt != this->Propals.end()))
+        {
+            if (currentPropal->isOverlappedWith(*tmpIt))
+            {
+                currentPropal->nOverlappedOri++;
+                tmpIt->nOverlappedOri++;
+            }
+            ++tmpIt;
+        }
+        currentPropal->isNew=false;
+        currentPropal->nOverlappedOptimized=currentPropal->nOverlappedOri;
+        ++ currentPropal;
+    }
+}
+
+void Propal_manager::optimize()
+{
+    VecPropal::iterator currentPropal=this->Propals.begin();
+    VecPropal::iterator tmpIt=this->Propals.begin();
+    this->maxOccurs=0;
+    while(currentPropal != this->Propals.end())
+    {
+        if (currentPropal->isOptimized)
+        {
+            if (!currentPropal->isOK)
+            {
+                tmpIt=currentPropal+1;
+                while((tmpIt->startDate<=currentPropal->endDate) && (tmpIt != this->Propals.end()))
+                {
+                    tmpIt->nOverlappedOptimized--;
+                    ++tmpIt;
+                }
+                currentPropal->nOverlappedOptimized=0;
+            }
+        }else
+        {
+            if (currentPropal->nOverlappedOptimized==0)
+            {
+                currentPropal->isOptimized=true;
+                currentPropal->isOK=true;
+                this->maxOccurs++;
+            }else
+            {
+                if (currentPropal == min_element(currentPropal,currentPropal+currentPropal->nOverlappedOptimized,FoncteurMinPropal()))
+                {
+                    // Current occur is the one between the next nOverlappedOptimised ones
+                    tmpIt=currentPropal+1;
+                    while((tmpIt->startDate<=currentPropal->endDate) && (tmpIt != this->Propals.end()))
+                    {
+                        tmpIt->isOptimized=true;
+                        tmpIt->isOK=false;
+                        ++tmpIt;
+                    }
+                    currentPropal->nOverlappedOptimized=0;
+                    currentPropal->isOptimized=true;
+                    currentPropal->isOK=true;
+                    this->maxOccurs++;
+                }else
+                {
+                    // current occur is not the best, so is ignored
+                    tmpIt=currentPropal+1;
+                    while((tmpIt->startDate<=currentPropal->endDate) && (tmpIt != this->Propals.end()))
+                    {
+                        tmpIt->nOverlappedOptimized--;
+                        ++tmpIt;
+                    }
+                    currentPropal->nOverlappedOptimized=0;
+                    currentPropal->isOptimized=true;
+                    currentPropal->isOK=false;
+                }
+            }
+        }
+        //cout << *currentPropal << endl;
+        ++currentPropal;
+    }
+}
+
+unsigned int Propal_manager::getBestNumber()
+{
+    if (this->hasChanged)
+    {
+        sort(this->Propals.begin(),this->Propals.end());
+        this->hasChanged=false;
+        this->calcOverlapped();
+        this->optimize();
+    }
+    return this->maxOccurs;
+}
+
+istream &operator>>(istream &flux, Propal_manager &p)
+{
+    unsigned int a,b;
+    flux >> a >> b;
+    p.addPropal(a,b);
+    return flux;
+}
+
+
+/* MAIN FUNCTION  --------------------------------------------- */
 int main()
 {
-    int nInput=0;
-    int startDate,duration,endDate;
-    unsigned short *tabDay;
-    int bOK=1;
-    int nTest=0;
+    unsigned int nInput;
+    Propal_manager manager;
 
     cin >> nInput;
     cin.ignore();
 
-    // Etape 1 : Stockage dans un map<int>
-
-    // Etape 2 : Tri par date de début
-
-    // Etape 3 : Filtre des doublons sur date de début (ne sont conservé que les durées les plus courtes)
-
-    // Etape 4 : Pondération des périodes
-
-    // Etape 5 : Filtre des périodes en chevauchement (ne sont conservé que celles qui ont le moins d'impact)
-
-    // Etape 6 : comptage d'occurrences
-
-    /*
-    for(int i=0;i<nInput;i++)
+    for(unsigned int i=0; i<nInput; i++)
     {
-        cin >> startDate >> duration;
-        endDate = startDate + duration -1;
+        cin >> manager;
         cin.ignore();
-        bOK=1;
-        for (int j=startDate;j<endDate;j++)
-        {
-            if (tabDay[j]!=0)
-            {
-                bOK=0;
-                break;
-            }
-        };
-        if (bOK)
-        {
-            for (int j=startDate;j<endDate;j++)
-            {
-                tabDay[j]=1;
-            }
-            nTest++;
-        }
     }
-    */
-    cout << nTest << endl;
+    cout << manager.getBestNumber() << endl;
+
     return 0;
 }
