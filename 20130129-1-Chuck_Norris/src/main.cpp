d133dfc633f022952cccee5feef170d50014e014
Benoit Dunand-Laisin <benoit.dunand_laisin@laposte.net>
2013-03-30 16:45:16 +0100

    Mise à jour de 20130129-1

--- 20130129-1-Chuck_Norris/src/main.cpp
+++ 20130129-1-Chuck_Norris/src/main.cpp
@@ -1,17 +1,60 @@
 // Read inputs from stdin. Write outputs to stdout.
-
 #include <iostream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
+ostream &chuck_conversion(ostream &out, const unsigned char c, unsigned char &tBit);
+ostream &chuck_conversion(ostream &out, istream &in);
+
+
+ostream &chuck_conversion(ostream &out, const unsigned char c, unsigned char &tBit)
+{
+    string bitView;
+
+    bitView = bitset<sizeof(unsigned char)*7>(c).to_string();
+    for(int i=0; i<7; i++)
+    {
+        if (tBit!=(unsigned char)bitView[i])
+        {
+            if ((tBit=='0') || (tBit=='1'))
+            {
+                out << " ";
+            }
+            tBit=(unsigned char)bitView[i];
+            if (tBit=='1')
+            {
+                out << "0 ";
+            }
+            else
+            {
+                out << "00 ";
+            }
+
+        }
+        out << "0";
+    }
+    return out;
+}
+
+ostream &chuck_conversion(ostream &out, istream &in)
+{
+    unsigned char c,tBit='\n';
+    while(!in.eof())
+    {
+        c = in.get();
+        if ((in.good()) && (c!='\n'))
+        {
+            chuck_conversion(out, c,tBit);
+        }
+    }
+    return out;
+}
+
 int main()
 {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {	
-		cout << "Hello World!" << endl;
-	}
-	
-	return 0;
+    chuck_conversion(cout, cin);
+    cout << endl;
+    return 0;
 }
